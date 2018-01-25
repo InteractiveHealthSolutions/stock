bb817c61ae1066b9dbf1a3ee097e77e4d84dd1bb
Hareem Ajaz <hareem.ajaz@ihsinformatics.com>
2018-01-25 11:14:50 +0500

    HA : ILR issue resolved

--- stock-web/src/main/java/com/ihs/stock/web/controller/ControllerUtility.java
+++ stock-web/src/main/java/com/ihs/stock/web/controller/ControllerUtility.java
@@ -385,6 +385,26 @@ public class ControllerUtility {
 			for (int i = 0; i < locationsObj.size(); i++) {
 				locations.add(locationsObj.get(i).getName());
 			}
+			if(months.size() < 1)
+			{
+				months.add(1);
+				months.add(2);
+				months.add(3);
+				months.add(4);
+				months.add(5);
+				months.add(6);
+				months.add(7);
+				months.add(8);
+				months.add(9);
+				months.add(10);
+				months.add(11);
+				months.add(12);
+			}
+			if(years.size() < 1)
+			{
+				years.add(2018);
+				years.add(2017);
+			}
 			modelAndView.addObject("months", months);
 			modelAndView.addObject("years", years);
 			modelAndView.addObject("locations", locations);
